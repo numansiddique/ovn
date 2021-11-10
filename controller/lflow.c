numansiddique/ovn

commit dd94f1266ca4f3c750bc59c474ea342ef3ff9983

northd: MAC learning: Add logical flows for fdb.
This patch adds the logical flows to do mac learning
for the logical ports whose port security is disabled
with 'unknown' addresses set.

Until now, OVN delivers the unknown destination packets to all
the logical ports with 'unknown' addresses set.  With this
mac learning feature, such flooding of packets is avoided once
OVN has learnt the port-to-mac bindings.

Note that we don't learn macs seen on the localnet logical ports.

Acked-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

controller/lflow.c
@@ -89,6 +89,11 @@ static void lflow_resource_destroy_lflow(struct lflow_resource_ref *,
 static bool
 lookup_port_cb(const void *aux_, const char *port_name, unsigned int *portp)
 {
+    if (!strcmp(port_name, "none")) {
+        *portp = 0;
+        return true;
+    }
+
     const struct lookup_port_aux *aux = aux_;
 
     const struct sbrec_port_binding *pb

controller/lflow.h
@@ -61,9 +61,9 @@ struct uuid;
  * you make any changes. */
 #define OFTABLE_PHY_TO_LOG            0
 #define OFTABLE_LOG_INGRESS_PIPELINE  8 /* First of LOG_PIPELINE_LEN tables. */
-#define OFTABLE_REMOTE_OUTPUT        32
-#define OFTABLE_LOCAL_OUTPUT         33
-#define OFTABLE_CHECK_LOOPBACK       34
+#define OFTABLE_REMOTE_OUTPUT        37
+#define OFTABLE_LOCAL_OUTPUT         38
+#define OFTABLE_CHECK_LOOPBACK       39
 #define OFTABLE_LOG_EGRESS_PIPELINE  40 /* First of LOG_PIPELINE_LEN tables. */
 #define OFTABLE_SAVE_INPORT          64
 #define OFTABLE_LOG_TO_PHY           65
@@ -74,9 +74,6 @@ struct uuid;
 #define OFTABLE_CT_SNAT_FOR_VIP      70
 #define OFTABLE_GET_FDB              71
 #define OFTABLE_LOOKUP_FDB           72
-
-/* The number of tables for the ingress and egress pipelines. */
-#define LOG_PIPELINE_LEN 24
 
 enum ref_type {
     REF_TYPE_ADDRSET,

lib/ovn-util.h
@@ -261,4 +261,7 @@ BUILD_ASSERT_DECL(SCTP_INIT_CHUNK_LEN == sizeof(struct sctp_init_chunk));
 /* See RFC 4960 Sections 3.3.7 and 8.5.1 for information on this flag. */
 #define SCTP_ABORT_CHUNK_FLAG_T (1 << 0)
 
+/* The number of tables for the ingress and egress pipelines. */
+#define LOG_PIPELINE_LEN 29
+
 #endif

northd/ovn-northd.8.xml
@@ -307,7 +307,73 @@
       </li>
     </ul>
 
-    <h3>Ingress Table 3: <code>from-lport</code> Pre-ACLs</h3>
+    <h3>Ingress Table 3: Lookup MAC address learning table</h3>
+
+    <p>
+      This table looks up the MAC learning table of the logical switch
+      datapath to check if the <code>port-mac</code> pair is present
+      or not. MAC is learnt only for logical switch VIF ports whose
+      port security is disabled and 'unknown' address set.
+    </p>
+
+    <ul>
+      <li>
+        <p>
+          For each such logical port <var>p</var> whose port security
+          is disabled and 'unknown' address set following flow
+          is added.
+        </p>
+
+        <ul>
+          <li>
+            Priority 100 flow with the match
+            <code>inport == <var>p</var></code> and action
+            <code>reg0[11] = lookup_fdb(inport, eth.src); next;</code>
+          </li>
+        </ul>
+      </li>
+
+      <li>
+        One priority-0 fallback flow that matches all packets and advances to
+        the next table.
+      </li>
+    </ul>
+
+    <h3>Ingress Table 4: Learn MAC of 'unknown' ports.</h3>
+
+    <p>
+      This table learns the MAC addresses seen on the logical ports
+      whose port security is disabled and 'unknown' address set
+      if the <code>lookup_fdb</code> action returned false in the
+      previous table.
+    </p>
+
+    <ul>
+      <li>
+        <p>
+          For each such logical port <var>p</var> whose port security
+          is disabled and 'unknown' address set following flow
+          is added.
+        </p>
+
+        <ul>
+          <li>
+            Priority 100 flow with the match
+            <code>inport == <var>p</var> &amp;&amp; reg0[11] == 0</code> and
+            action <code>put_fdb(inport, eth.src); next;</code> which stores
+            the <code>port-mac</code> in the mac learning table of the
+            logical switch datapath and advances the packet to the next table.
+          </li>
+        </ul>
+      </li>
+
+      <li>
+        One priority-0 fallback flow that matches all packets and advances to
+        the next table.
+      </li>
+    </ul>
+
+    <h3>Ingress Table 5: <code>from-lport</code> Pre-ACLs</h3>
 
     <p>
       This table prepares flows for possible stateful ACL processing in
@@ -332,7 +398,7 @@
       db="OVN_Northbound"/> table.
     </p>
 
-    <h3>Ingress Table 4: Pre-LB</h3>
+    <h3>Ingress Table 6: Pre-LB</h3>
 
     <p>
       This table prepares flows for possible stateful load balancing processing
@@ -399,7 +465,7 @@
       logical router datapath to logical switch datapath.
     </p>
 
-    <h3>Ingress Table 5: Pre-stateful</h3>
+    <h3>Ingress Table 7: Pre-stateful</h3>
 
     <p>
       This table prepares flows for all possible stateful processing
@@ -410,7 +476,7 @@
       <code>ct_next;</code> action.
     </p>
 
-    <h3>Ingress Table 6: <code>from-lport</code> ACL hints</h3>
+    <h3>Ingress Table 8: <code>from-lport</code> ACL hints</h3>
 
     <p>
       This table consists of logical flows that set hints
@@ -490,7 +556,7 @@
       </li>
     </ul>
 
-    <h3>Ingress table 7: <code>from-lport</code> ACLs</h3>
+    <h3>Ingress table 9: <code>from-lport</code> ACLs</h3>
 
     <p>
       Logical flows in this table closely reproduce those in the
@@ -599,7 +665,7 @@
       </li>
     </ul>
 
-    <h3>Ingress Table 8: <code>from-lport</code> QoS Marking</h3>
+    <h3>Ingress Table 10: <code>from-lport</code> QoS Marking</h3>
 
     <p>
       Logical flows in this table closely reproduce those in the
@@ -621,7 +687,7 @@
       </li>
     </ul>
 
-    <h3>Ingress Table 9: <code>from-lport</code> QoS Meter</h3>
+    <h3>Ingress Table 11: <code>from-lport</code> QoS Meter</h3>
 
     <p>
       Logical flows in this table closely reproduce those in the
@@ -643,7 +709,7 @@
       </li>
     </ul>
 
-    <h3>Ingress Table 10: LB</h3>
+    <h3>Ingress Table 12: LB</h3>
 
     <p>
       It contains a priority-0 flow that simply moves traffic to the next
@@ -669,7 +735,7 @@
       connection.)
     </p>
 
-    <h3>Ingress Table 11: Stateful</h3>
+    <h3>Ingress Table 13: Stateful</h3>
 
     <ul>
       <li>
@@ -727,7 +793,7 @@
       </li>
     </ul>
 
-    <h3>Ingress Table 12: Pre-Hairpin</h3>
+    <h3>Ingress Table 14: Pre-Hairpin</h3>
     <ul>
       <li>
         If the logical switch has load balancer(s) configured, then a
@@ -752,7 +818,7 @@
       </li>
     </ul>
 
-    <h3>Ingress Table 13: Nat-Hairpin</h3>
+    <h3>Ingress Table 15: Nat-Hairpin</h3>
     <ul>
       <li>
          If the logical switch has load balancer(s) configured, then a
@@ -778,7 +844,7 @@
       </li>
     </ul>
 
-    <h3>Ingress Table 14: Hairpin</h3>
+    <h3>Ingress Table 16: Hairpin</h3>
     <ul>
       <li>
         A priority-1 flow that hairpins traffic matched by non-default
@@ -791,7 +857,7 @@
       </li>
     </ul>
 
-    <h3>Ingress Table 15: ARP/ND responder</h3>
+    <h3>Ingress Table 17: ARP/ND responder</h3>
 
     <p>
       This table implements ARP/ND responder in a logical switch for known
@@ -1081,7 +1147,7 @@ output;
       </li>
     </ul>
 
-    <h3>Ingress Table 16: DHCP option processing</h3>
+    <h3>Ingress Table 18: DHCP option processing</h3>
 
     <p>
       This table adds the DHCPv4 options to a DHCPv4 packet from the
@@ -1142,7 +1208,7 @@ next;
       </li>
     </ul>
 
-    <h3>Ingress Table 17: DHCP responses</h3>
+    <h3>Ingress Table 19: DHCP responses</h3>
 
     <p>
       This table implements DHCP responder for the DHCP replies generated by
@@ -1223,7 +1289,7 @@ output;
       </li>
     </ul>
 
-    <h3>Ingress Table 18 DNS Lookup</h3>
+    <h3>Ingress Table 20 DNS Lookup</h3>
 
     <p>
       This table looks up and resolves the DNS names to the corresponding
@@ -1252,7 +1318,7 @@ reg0[4] = dns_lookup(); next;
       </li>
     </ul>
 
-    <h3>Ingress Table 19 DNS Responses</h3>
+    <h3>Ingress Table 21 DNS Responses</h3>
 
     <p>
       This table implements DNS responder for the DNS replies generated by
@@ -1287,7 +1353,7 @@ output;
       </li>
     </ul>
 
-    <h3>Ingress table 20 External ports</h3>
+    <h3>Ingress table 22 External ports</h3>
 
     <p>
       Traffic from the <code>external</code> logical ports enter the ingress
@@ -1330,7 +1396,7 @@ output;
       </li>
     </ul>
 
-    <h3>Ingress Table 21 Destination Lookup</h3>
+    <h3>Ingress Table 23 Destination Lookup</h3>
 
     <p>
       This table implements switching behavior.  It contains these logical
@@ -1493,12 +1559,58 @@ output;
       </li>
 
       <li>
-        One priority-0 fallback flow that matches all packets and outputs them
-        to the <code>MC_UNKNOWN</code> multicast group, which
-        <code>ovn-northd</code> populates with all enabled logical ports that
-        accept unknown destination packets.  As a small optimization, if no
-        logical ports accept unknown destination packets,
-        <code>ovn-northd</code> omits this multicast group and logical flow.
+        One priority-0 fallback flow that matches all packets with the
+        action <code>outport = get_fdb(eth.dst); next;</code>. The action
+        <code>get_fdb</code> gets the port for the <code>eth.dst</code>
+        in the MAC learning table of the logical switch datapath. If there
+        is no entry for <code>eth.dst</code> in the MAC learning table,
+        then it stores <code>none</code> in the <code>outport</code>.
+      </li>
+    </ul>
+
+    <h3>Ingress Table 23 Destination unknown</h3>
+
+    <p>
+      This table handles the packets whose destination was not found or
+      and looked up in the MAC learning table of the logical switch
+      datapath. It contains the following flows.
+    </p>
+
+    <ul>
+      <li>
+        <p>
+          If the logical switch has logical ports with 'unknown' addresses set,
+          then the below logical flow is added
+        </p>
+
+        <ul>
+          <li>
+            Priority 50 flow with the match <code>outport == none</code> then
+            outputs them to the <code>MC_UNKNOWN</code> multicast group, which
+            <code>ovn-northd</code> populates with all enabled logical ports
+            that accept unknown destination packets.  As a small optimization,
+            if no logical ports accept unknown destination packets,
+            <code>ovn-northd</code> omits this multicast group and logical
+            flow.
+          </li>
+        </ul>
+
+        <p>
+          If the logical switch has no logical ports with 'unknown' address
+          set, then the below logical flow is added
+        </p>
+
+        <ul>
+          <li>
+            Priority 50 flow with the match <code>outport == none</code>
+            and drops the packets.
+          </li>
+        </ul>
+      </li>
+
+      <li>
+        One priority-0 fallback flow that outputs the packet to the egress
+        stage with the outport learnt from <code>get_fdb</code> action.
       </li>
     </ul>
 

northd/ovn-northd.c
@@ -138,25 +138,28 @@ enum ovn_stage {
     PIPELINE_STAGE(SWITCH, IN,  PORT_SEC_L2,    0, "ls_in_port_sec_l2")   \
     PIPELINE_STAGE(SWITCH, IN,  PORT_SEC_IP,    1, "ls_in_port_sec_ip")   \
     PIPELINE_STAGE(SWITCH, IN,  PORT_SEC_ND,    2, "ls_in_port_sec_nd")   \
-    PIPELINE_STAGE(SWITCH, IN,  PRE_ACL,        3, "ls_in_pre_acl")       \
-    PIPELINE_STAGE(SWITCH, IN,  PRE_LB,         4, "ls_in_pre_lb")        \
-    PIPELINE_STAGE(SWITCH, IN,  PRE_STATEFUL,   5, "ls_in_pre_stateful")  \
-    PIPELINE_STAGE(SWITCH, IN,  ACL_HINT,       6, "ls_in_acl_hint")      \
-    PIPELINE_STAGE(SWITCH, IN,  ACL,            7, "ls_in_acl")           \
-    PIPELINE_STAGE(SWITCH, IN,  QOS_MARK,       8, "ls_in_qos_mark")      \
-    PIPELINE_STAGE(SWITCH, IN,  QOS_METER,      9, "ls_in_qos_meter")     \
-    PIPELINE_STAGE(SWITCH, IN,  LB,            10, "ls_in_lb")            \
-    PIPELINE_STAGE(SWITCH, IN,  STATEFUL,      11, "ls_in_stateful")      \
-    PIPELINE_STAGE(SWITCH, IN,  PRE_HAIRPIN,   12, "ls_in_pre_hairpin")   \
-    PIPELINE_STAGE(SWITCH, IN,  NAT_HAIRPIN,   13, "ls_in_nat_hairpin")       \
-    PIPELINE_STAGE(SWITCH, IN,  HAIRPIN,       14, "ls_in_hairpin")       \
-    PIPELINE_STAGE(SWITCH, IN,  ARP_ND_RSP,    15, "ls_in_arp_rsp")       \
-    PIPELINE_STAGE(SWITCH, IN,  DHCP_OPTIONS,  16, "ls_in_dhcp_options")  \
-    PIPELINE_STAGE(SWITCH, IN,  DHCP_RESPONSE, 17, "ls_in_dhcp_response") \
-    PIPELINE_STAGE(SWITCH, IN,  DNS_LOOKUP,    18, "ls_in_dns_lookup")    \
-    PIPELINE_STAGE(SWITCH, IN,  DNS_RESPONSE,  19, "ls_in_dns_response")  \
-    PIPELINE_STAGE(SWITCH, IN,  EXTERNAL_PORT, 20, "ls_in_external_port") \
-    PIPELINE_STAGE(SWITCH, IN,  L2_LKUP,       21, "ls_in_l2_lkup")       \
+    PIPELINE_STAGE(SWITCH, IN,  LOOKUP_FDB ,    3, "ls_in_lookup_fdb")    \
+    PIPELINE_STAGE(SWITCH, IN,  PUT_FDB,        4, "ls_in_put_fdb")       \
+    PIPELINE_STAGE(SWITCH, IN,  PRE_ACL,        5, "ls_in_pre_acl")       \
+    PIPELINE_STAGE(SWITCH, IN,  PRE_LB,         6, "ls_in_pre_lb")        \
+    PIPELINE_STAGE(SWITCH, IN,  PRE_STATEFUL,   7, "ls_in_pre_stateful")  \
+    PIPELINE_STAGE(SWITCH, IN,  ACL_HINT,       8, "ls_in_acl_hint")      \
+    PIPELINE_STAGE(SWITCH, IN,  ACL,            9, "ls_in_acl")           \
+    PIPELINE_STAGE(SWITCH, IN,  QOS_MARK,      10, "ls_in_qos_mark")      \
+    PIPELINE_STAGE(SWITCH, IN,  QOS_METER,     11, "ls_in_qos_meter")     \
+    PIPELINE_STAGE(SWITCH, IN,  LB,            12, "ls_in_lb")            \
+    PIPELINE_STAGE(SWITCH, IN,  STATEFUL,      13, "ls_in_stateful")      \
+    PIPELINE_STAGE(SWITCH, IN,  PRE_HAIRPIN,   14, "ls_in_pre_hairpin")   \
+    PIPELINE_STAGE(SWITCH, IN,  NAT_HAIRPIN,   15, "ls_in_nat_hairpin")   \
+    PIPELINE_STAGE(SWITCH, IN,  HAIRPIN,       16, "ls_in_hairpin")       \
+    PIPELINE_STAGE(SWITCH, IN,  ARP_ND_RSP,    17, "ls_in_arp_rsp")       \
+    PIPELINE_STAGE(SWITCH, IN,  DHCP_OPTIONS,  18, "ls_in_dhcp_options")  \
+    PIPELINE_STAGE(SWITCH, IN,  DHCP_RESPONSE, 19, "ls_in_dhcp_response") \
+    PIPELINE_STAGE(SWITCH, IN,  DNS_LOOKUP,    20, "ls_in_dns_lookup")    \
+    PIPELINE_STAGE(SWITCH, IN,  DNS_RESPONSE,  21, "ls_in_dns_response")  \
+    PIPELINE_STAGE(SWITCH, IN,  EXTERNAL_PORT, 22, "ls_in_external_port") \
+    PIPELINE_STAGE(SWITCH, IN,  L2_LKUP,       23, "ls_in_l2_lkup")       \
+    PIPELINE_STAGE(SWITCH, IN,  L2_UNKNOWN,    24, "ls_in_l2_unknown")    \
                                                                           \
     /* Logical switch egress stages. */                                   \
     PIPELINE_STAGE(SWITCH, OUT, PRE_LB,       0, "ls_out_pre_lb")         \
@@ -223,6 +226,7 @@ enum ovn_stage {
 #define REGBIT_ACL_HINT_ALLOW     "reg0[8]"
 #define REGBIT_ACL_HINT_DROP      "reg0[9]"
 #define REGBIT_ACL_HINT_BLOCK     "reg0[10]"
+#define REGBIT_LKUP_FDB           "reg0[11]"
 
 /* Register definitions for switches and routers. */
 
@@ -4743,6 +4747,42 @@ build_lswitch_input_port_sec_od(
     }
 }
 
+static void
+build_lswitch_learn_fdb_op(
+        struct ovn_port *op, struct hmap *lflows,
+        struct ds *actions, struct ds *match)
+{
+    if (op->nbsp && !op->n_ps_addrs && !strcmp(op->nbsp->type, "") &&
+        op->has_unknown) {
+        ds_clear(match);
+        ds_clear(actions);
+        ds_put_format(match, "inport == %s", op->json_key);
+        ds_put_format(actions, REGBIT_LKUP_FDB
+                      " = lookup_fdb(inport, eth.src); next;");
+        ovn_lflow_add_with_hint(lflows, op->od, S_SWITCH_IN_LOOKUP_FDB, 100,
+                                ds_cstr(match), ds_cstr(actions),
+                                &op->nbsp->header_);
+
+        ds_put_cstr(match, " && "REGBIT_LKUP_FDB" == 0");
+        ds_clear(actions);
+        ds_put_cstr(actions, "put_fdb(inport, eth.src); next;");
+        ovn_lflow_add_with_hint(lflows, op->od, S_SWITCH_IN_PUT_FDB, 100,
+                                ds_cstr(match), ds_cstr(actions),
+                                &op->nbsp->header_);
+    }
+}
+
+static void
+build_lswitch_learn_fdb_od(
+        struct ovn_datapath *od, struct hmap *lflows)
+{
+
+    if (od->nbs) {
+        ovn_lflow_add(lflows, od, S_SWITCH_IN_LOOKUP_FDB, 0, "1", "next;");
+        ovn_lflow_add(lflows, od, S_SWITCH_IN_PUT_FDB, 0, "1", "next;");
+    }
+}
+
 /* Egress table 8: Egress port security - IP (priorities 90 and 80)
  * if port security enabled.
  *
@@ -6600,16 +6640,25 @@ build_lswitch_flows(struct hmap *datapaths, struct hmap *lflows)
     struct ds actions = DS_EMPTY_INITIALIZER;
     struct ovn_datapath *od;
 
-    /* Ingress table 19: Destination lookup for unknown MACs (priority 0). */
+    /* Ingress table 24: Destination lookup for unknown MACs (priority 0). */
     HMAP_FOR_EACH (od, key_node, datapaths) {
         if (!od->nbs) {
             continue;
         }
 
+        ovn_lflow_add(lflows, od, S_SWITCH_IN_L2_LKUP, 0, "1",
+                      "outport = get_fdb(eth.dst); next;");
+
         if (od->has_unknown) {
-            ovn_lflow_add_unique(lflows, od, S_SWITCH_IN_L2_LKUP, 0, "1",
-                                 "outport = \""MC_UNKNOWN"\"; output;");
+            ovn_lflow_add(lflows, od, S_SWITCH_IN_L2_UNKNOWN, 50,
+                          "outport == \"none\"",
+                          "outport = \""MC_UNKNOWN"\"; output;");
+        } else {
+            ovn_lflow_add(lflows, od, S_SWITCH_IN_L2_UNKNOWN, 50,
+                          "outport == \"none\"", "drop;");
         }
+        ovn_lflow_add(lflows, od, S_SWITCH_IN_L2_UNKNOWN, 0, "1",
+                      "output;");
     }
 
     ds_destroy(&match);
@@ -11513,6 +11562,7 @@ build_lswitch_and_lrouter_iterate_by_od(struct ovn_datapath *od,
     build_fwd_group_lflows(od, lsi->lflows);
     build_lswitch_lflows_admission_control(od, lsi->lflows);
     build_lswitch_input_port_sec_od(od, lsi->lflows);
+    build_lswitch_learn_fdb_od(od, lsi->lflows);
     build_lswitch_arp_nd_responder_default(od, lsi->lflows);
     build_lswitch_dns_lookup_and_response(od, lsi->lflows);
     build_lswitch_dhcp_and_dns_defaults(od, lsi->lflows);
@@ -11552,6 +11602,8 @@ build_lswitch_and_lrouter_iterate_by_op(struct ovn_port *op,
     /* Build Logical Switch Flows. */
     build_lswitch_input_port_sec_op(op, lsi->lflows, &lsi->actions,
                                     &lsi->match);
+    build_lswitch_learn_fdb_op(op, lsi->lflows, &lsi->actions,
+                               &lsi->match);
     build_lswitch_arp_nd_responder_skip_local(op, lsi->lflows,
                                               &lsi->match);
     build_lswitch_arp_nd_responder_known_ips(op, lsi->lflows,

ovn-sb.ovsschema
@@ -1,7 +1,7 @@
 {
     "name": "OVN_Southbound",
-    "version": "20.16.0",
-    "cksum": "1219580357 26536",
+    "version": "20.16.1",
+    "cksum": "4243908307 26536",
     "tables": {
         "SB_Global": {
             "columns": {
@@ -103,7 +103,7 @@
                                                        "egress"]]}}},
                 "table_id": {"type": {"key": {"type": "integer",
                                               "minInteger": 0,
-                                              "maxInteger": 23}}},
+                                              "maxInteger": 32}}},
                 "priority": {"type": {"key": {"type": "integer",
                                               "minInteger": 0,
                                               "maxInteger": 65535}}},

commit 69d7ee42c7f70d9f5ba1b560a3f34ea0d095403b

controller I-P: Add missing FDB flows when a datapath is added locally.
Reported-at: https://bugzilla.redhat.com/show_bug.cgi?id=1983111
Fixes: dd94f1266ca4("northd: MAC learning: Add logical flows for fdb.")
Signed-off-by: Numan Siddique <numans@ovn.org>

controller/lflow.c
@@ -1979,6 +1979,18 @@ lflow_add_flows_for_datapath(const struct sbrec_datapath_binding *dp,
             }
         }
     }
+
+    struct sbrec_fdb *fdb_index_row =
+        sbrec_fdb_index_init_row(l_ctx_in->sbrec_fdb_by_dp_key);
+    sbrec_fdb_index_set_dp_key(fdb_index_row, dp->tunnel_key);
+    const struct sbrec_fdb *fdb_row;
+    SBREC_FDB_FOR_EACH_EQUAL (fdb_row, fdb_index_row,
+                              l_ctx_in->sbrec_fdb_by_dp_key) {
+        consider_fdb_flows(fdb_row, l_ctx_in->local_datapaths,
+                           l_ctx_out->flow_table);
+    }
+    sbrec_fdb_index_destroy_row(fdb_index_row);
+
 lflow_processing_end:
     sbrec_logical_flow_index_destroy_row(lf_row);
 

controller/lflow.h
@@ -131,6 +131,7 @@ struct lflow_ctx_in {
     struct ovsdb_idl_index *sbrec_logical_flow_by_logical_datapath;
     struct ovsdb_idl_index *sbrec_logical_flow_by_logical_dp_group;
     struct ovsdb_idl_index *sbrec_port_binding_by_name;
+    struct ovsdb_idl_index *sbrec_fdb_by_dp_key;
     const struct sbrec_port_binding_table *port_binding_table;
     const struct sbrec_dhcp_options_table *dhcp_options_table;
     const struct sbrec_dhcpv6_options_table *dhcpv6_options_table;

controller/ovn-controller.c
@@ -2205,6 +2205,11 @@ init_lflow_ctx(struct engine_node *node,
                 engine_get_input("SB_multicast_group", node),
                 "name_datapath");
 
+    struct ovsdb_idl_index *sbrec_fdb_by_dp_key =
+        engine_ovsdb_node_get_index(
+                engine_get_input("SB_fdb", node),
+                "dp_key");
+
     struct sbrec_port_binding_table *port_binding_table =
         (struct sbrec_port_binding_table *)EN_OVSDB_GET(
             engine_get_input("SB_port_binding", node));
@@ -2272,6 +2277,7 @@ init_lflow_ctx(struct engine_node *node,
     l_ctx_in->sbrec_logical_flow_by_logical_dp_group =
         sbrec_logical_flow_by_dp_group;
     l_ctx_in->sbrec_port_binding_by_name = sbrec_port_binding_by_name;
+    l_ctx_in->sbrec_fdb_by_dp_key = sbrec_fdb_by_dp_key;
     l_ctx_in->port_binding_table = port_binding_table;
     l_ctx_in->dhcp_options_table  = dhcp_table;
     l_ctx_in->dhcpv6_options_table = dhcpv6_table;
@@ -3146,6 +3152,9 @@ main(int argc, char *argv[])
         = ip_mcast_index_create(ovnsb_idl_loop.idl);
     struct ovsdb_idl_index *sbrec_igmp_group
         = igmp_group_index_create(ovnsb_idl_loop.idl);
+    struct ovsdb_idl_index *sbrec_fdb_by_dp_key
+        = ovsdb_idl_index_create1(ovnsb_idl_loop.idl,
+                                  &sbrec_fdb_col_dp_key);
     struct ovsdb_idl_index *sbrec_fdb_by_dp_key_mac
         = ovsdb_idl_index_create2(ovnsb_idl_loop.idl,
                                   &sbrec_fdb_col_mac,
@@ -3364,6 +3373,8 @@ main(int argc, char *argv[])
                                 sbrec_port_binding_by_datapath);
     engine_ovsdb_node_add_index(&en_sb_datapath_binding, "key",
                                 sbrec_datapath_binding_by_key);
+    engine_ovsdb_node_add_index(&en_sb_fdb, "dp_key",
+                                sbrec_fdb_by_dp_key);
 
     struct ed_type_lflow_output *lflow_output_data =
         engine_get_internal_data(&en_lflow_output);

tests/ovn.at
@@ -26660,7 +26660,7 @@ OVN_CLEANUP([hv1])
 AT_CLEANUP
 ])
 
-AT_SETUP([OVN FDB (MAC learning) - 2 HVs, 2 LS, 1 LR ])
+AT_SETUP([OVN FDB (MAC learning) - 3 HVs, 2 LS, 1 LR ])
 ovn_start
 
 # Create the first logical switch with one port
@@ -26737,6 +26737,21 @@ ovs-vsctl -- add-port br-int hv2-vif2 -- \
     set interface hv2-vif2 external-ids:iface-id=sw1-p1 \
     options:tx_pcap=hv2/vif2-tx.pcap \
     options:rxq_pcap=hv2/vif2-rx.pcap \
+    ofport-request=2
+
+sim_add hv3
+as hv3
+ovs-vsctl add-br br-phys
+ovn_attach n1 br-phys 192.168.0.3
+ovs-vsctl -- add-port br-int hv3-vif1 -- \
+    set interface hv3-vif1 external-ids:iface-id=sw0-p4 \
+    options:tx_pcap=hv3/vif1-tx.pcap \
+    options:rxq_pcap=hv3/vif1-rx.pcap \
+    ofport-request=1
+ovs-vsctl -- add-port br-int hv3-vif2 -- \
+    set interface hv3-vif2 external-ids:iface-id=sw1-p3 \
+    options:tx_pcap=hv3/vif2-tx.pcap \
+    options:rxq_pcap=hv3/vif2-rx.pcap \
     ofport-request=2
 
 OVN_POPULATE_ARP
@@ -26851,6 +26866,30 @@ AT_CHECK([cat hv2_offlows_table72.txt | grep -v NXST | cut -d ' ' -f8- | sort], 
 priority=100,reg14=0x1,metadata=0x1,dl_src=50:54:00:00:00:13 actions=load:0x1->NXM_NX_REG10[[8]]
 ])
 
+# Create the logical port sw0-p4 and this should be claimed by
+# hv3 and it should install the fdb flows in table 71 and 72.
+
+check ovn-nbctl lsp-add sw0 sw0-p4
+wait_for_ports_up sw0-p4
+
+#as hv3 ovn-appctl -t ovn-controller recompute
+check ovn-nbctl --wait=hv sync
+
+as hv3 ovs-ofctl dump-flows br-int table=71 > hv3_offlows_table71.txt
+as hv3 ovs-ofctl dump-flows br-int table=72 > hv3_offlows_table72.txt
+
+
+AT_CAPTURE_FILE([hv3_offlows_table71.txt])
+AT_CAPTURE_FILE([hv3_offlows_table72.txt])
+
+AT_CHECK([cat hv3_offlows_table71.txt | grep -v NXST | cut -d ' ' -f8- | sort], [0], [dnl
+priority=100,metadata=0x1,dl_dst=50:54:00:00:00:13 actions=load:0x1->NXM_NX_REG15[[]]
+])
+
+AT_CHECK([cat hv3_offlows_table72.txt | grep -v NXST | cut -d ' ' -f8- | sort], [0], [dnl
+priority=100,reg14=0x1,metadata=0x1,dl_src=50:54:00:00:00:13 actions=load:0x1->NXM_NX_REG10[[8]]
+])
+
 # Use the src mac 50:54:00:00:00:14 instead of 50:54:00:00:00:03
 src_mac=505400000014
 src_ip=$(ip_to_hex 10 0 0 14)
@@ -26867,12 +26906,14 @@ check_column "50:54:00:00:00:13 50:54:00:00:00:14" fdb mac
 check_column "$sw0_dpkey $sw0_dpkey" fdb dp_key
 check_column "$sw0p1_dpkey $sw0p1_dpkey" fdb port_key
 
-# Make sure that OVS tables 71 and 72 are populated on both hv1 and hv2.
+# Make sure that OVS tables 71 and 72 are populated on hv1, hv2 and hv3.
 as hv1 ovs-ofctl dump-flows br-int table=71 > hv1_offlows_table71.txt
 as hv2 ovs-ofctl dump-flows br-int table=71 > hv2_offlows_table71.txt
+as hv3 ovs-ofctl dump-flows br-int table=71 > hv3_offlows_table71.txt
 
 AT_CAPTURE_FILE([hv1_offlows_table71.txt])
 AT_CAPTURE_FILE([hv2_offlows_table71.txt])
+AT_CAPTURE_FILE([hv3_offlows_table71.txt])
 AT_CHECK([cat hv1_offlows_table71.txt | grep -v NXST | cut -d ' ' -f8- | sort], [0], [dnl
 priority=100,metadata=0x1,dl_dst=50:54:00:00:00:13 actions=load:0x1->NXM_NX_REG15[[]]
 priority=100,metadata=0x1,dl_dst=50:54:00:00:00:14 actions=load:0x1->NXM_NX_REG15[[]]
@@ -26883,17 +26924,29 @@ priority=100,metadata=0x1,dl_dst=50:54:00:00:00:13 actions=load:0x1->NXM_NX_REG1
 priority=100,metadata=0x1,dl_dst=50:54:00:00:00:14 actions=load:0x1->NXM_NX_REG15[[]]
 ])
 
+AT_CHECK([cat hv3_offlows_table71.txt | grep -v NXST | cut -d ' ' -f8- | sort], [0], [dnl
+priority=100,metadata=0x1,dl_dst=50:54:00:00:00:13 actions=load:0x1->NXM_NX_REG15[[]]
+priority=100,metadata=0x1,dl_dst=50:54:00:00:00:14 actions=load:0x1->NXM_NX_REG15[[]]
+])
+
 as hv1 ovs-ofctl dump-flows br-int table=72 > hv1_offlows_table72.txt
 as hv2 ovs-ofctl dump-flows br-int table=72 > hv2_offlows_table72.txt
+as hv3 ovs-ofctl dump-flows br-int table=72 > hv3_offlows_table72.txt
 
 AT_CAPTURE_FILE([hv1_offlows_table72.txt])
 AT_CAPTURE_FILE([hv2_offlows_table72.txt])
+AT_CAPTURE_FILE([hv3_offlows_table72.txt])
 AT_CHECK([cat hv1_offlows_table72.txt | grep -v NXST | cut -d ' ' -f8- | sort], [0], [dnl
 priority=100,reg14=0x1,metadata=0x1,dl_src=50:54:00:00:00:13 actions=load:0x1->NXM_NX_REG10[[8]]
 priority=100,reg14=0x1,metadata=0x1,dl_src=50:54:00:00:00:14 actions=load:0x1->NXM_NX_REG10[[8]]
 ])
 
 AT_CHECK([cat hv2_offlows_table72.txt | grep -v NXST | cut -d ' ' -f8- | sort], [0], [dnl
+priority=100,reg14=0x1,metadata=0x1,dl_src=50:54:00:00:00:13 actions=load:0x1->NXM_NX_REG10[[8]]
+priority=100,reg14=0x1,metadata=0x1,dl_src=50:54:00:00:00:14 actions=load:0x1->NXM_NX_REG10[[8]]
+])
+
+AT_CHECK([cat hv3_offlows_table72.txt | grep -v NXST | cut -d ' ' -f8- | sort], [0], [dnl
 priority=100,reg14=0x1,metadata=0x1,dl_src=50:54:00:00:00:13 actions=load:0x1->NXM_NX_REG10[[8]]
 priority=100,reg14=0x1,metadata=0x1,dl_src=50:54:00:00:00:14 actions=load:0x1->NXM_NX_REG10[[8]]
 ])
@@ -27080,7 +27133,7 @@ AT_CHECK([cat hv1_offlows_table72.txt | grep -v NXST], [1], [dnl
 AT_CHECK([cat hv2_offlows_table72.txt | grep -v NXST], [1], [dnl
 ])
 
-OVN_CLEANUP([hv1], [hv2])
+OVN_CLEANUP([hv1], [hv2], [hv3])
 AT_CLEANUP
 ])
 