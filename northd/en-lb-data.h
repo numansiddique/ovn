660e3896a434e577076a78567f8034fa429bb41a
Numan Siddique <numans@ovn.org>
2023-06-13 15:03:43 -0400

    northd: Add initial I-P for load balancer and load balancer groups
    Any changes to load balancers and load balancer groups
    are handled incrementally in the newly added 'lb_data'
    engine node.  'lb_data' is input to 'northd' node
    and the handler - northd_lb_data_handler in 'northd'
    node handles the changes.
    
    If a load balancer or load balancer group is associated to
    a logical switch or router then 'northd' node falls back
    to full recompute.  Upcoming patch will handle this scenario.
    
    Signed-off-by: Numan Siddique <numans@ovn.org>

--- northd/en-lb-data.h
+++ northd/en-lb-data.h
@@ -4,16 +4,44 @@
 #include <config.h>
 
 #include "openvswitch/hmap.h"
+#include "include/openvswitch/list.h"
+#include "lib/hmapx.h"
 
 #include "lib/inc-proc-eng.h"
+
+struct ovn_northd_lb;
+struct ovn_lb_group;
+
+struct tracked_lb_data {
+    /* Both created and updated lbs. hmapx node is 'struct ovn_northd_lb *'. */
+    struct hmapx crupdated_lbs;
+    struct hmapx deleted_lbs;
+
+    /* Both created and updated lb_groups. hmapx node is
+     * 'struct ovn_lb_group *'. */
+    struct hmapx crupdated_lb_groups;
+    struct hmapx deleted_lb_groups;
+
+    bool has_health_checks;
+};
 
 struct lb_data {
     struct hmap lbs;
     struct hmap lb_groups;
+
+    /* tracked data*/
+    bool tracked;
+    struct tracked_lb_data tracked_lb_data;
 };
 
 void *en_lb_data_init(struct engine_node *, struct engine_arg *);
 void en_lb_data_run(struct engine_node *, void *data);
 void en_lb_data_cleanup(void *data);
+void en_lb_data_clear_tracked_data(void *data);
+
+bool lb_data_load_balancer_handler(struct engine_node *,
+                                          void *data);
+bool lb_data_load_balancer_group_handler(struct engine_node *,
+                                                void *data);
 
 #endif /* end of EN_NORTHD_LB_DATA_H */
