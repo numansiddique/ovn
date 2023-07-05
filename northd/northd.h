1a461d1df48d4fc3001ee8743a9238379f9d7436
Numan Siddique <numans@ovn.org>
2023-07-05 15:39:45 +0530

    northd: Use objdep mgr for lport to lflow references.
    Instead of maintaing the lport to lflow references using
    'struct lflow_ref_list', this patch makes use of the existing
    objdep APIs.  Since objdep_mgr is not thread safe, an instance
    of objdep_mgr is maintained for each 'ovn_port'.
    
    Once we add the thread safe support to objdep APIs we can move
    the objdep_mgr to 'lflow' engine date.
    
    Using objdep APIs does come with a cost in memory.  We now
    maintain an additional hmap of ovn_lflow's to look up using
    uuid.  But this will be useful to handle datapath and load
    balancer changes in the lflow engine node.
    
    This patch does few more changes which are significant:
      -  Logical flows are now hashed without the logical
         datapaths.  If a logical flow is referenced by just one
         datapath, we don't rehash it.
    
      -  The synthetic 'hash' column of sbrec_logical_flow now
         doesn't use the logical datapath too.  This means that
         when ovn-northd is updated/upgraded and has this commit,
         all the logical flows with 'logical_datapath' column
         set will get deleted and re-added causing some disruptions.
    
      -  With the commit [1] which added I-P support for logical
         port changes, multiple logical flows with same match 'M'
         and actions 'A' are generated and stored without the
         dp groups, which was not the case prior to
         that patch.
         One example to generate these lflows is:
                 ovn-nbctl lsp-set-addresses sw0p1 "MAC1 IP1"
                 ovn-nbctl lsp-set-addresses sw1p1 "MAC1 IP1"
    	     ovn-nbctl lsp-set-addresses sw2p1 "MAC1 IP1"
    
         Now with this patch we go back to the earlier way.  i.e
         one logical flow with logical_dp_groups set.
    
      -  With this patch any updates to a logical port which
         doesn't result in new logical flows will not result in
         deletion and addition of same logical flows.
         Eg.
         ovn-nbctl set logical_switch_port sw0p1 external_ids:foo=bar
         will be a no-op to the SB logical flow table.
    
    [1] - 8bbd67891f68("northd: Incremental processing of VIF additions in 'lflow' node.")
    
    Suggested-by: Dumitru Ceara <dceara@redhat.com>
    Signed-off-by: Numan Siddique <numans@ovn.org>

--- northd/northd.h
+++ northd/northd.h
@@ -19,6 +19,7 @@
 #include "lib/ovn-sb-idl.h"
 #include "lib/ovn-util.h"
 #include "lib/ovs-atomic.h"
+#include "lib/objdep.h"
 #include "lib/sset.h"
 #include "northd/ipam.h"
 #include "openvswitch/hmap.h"
@@ -127,7 +128,10 @@ struct northd_data {
 };
 
 struct lflow_data {
-    struct hmap lflows;
+    struct hmap lflows_match_map;
+    struct hmap lflows_hash_map;
+    struct hmap ls_dp_groups;
+    struct hmap lr_dp_groups;
 };
 
 void lflow_data_init(struct lflow_data *);
@@ -354,10 +358,10 @@ void northd_indices_create(struct northd_data *data,
                            struct ovsdb_idl *ovnsb_idl);
 void build_lflows(struct ovsdb_idl_txn *ovnsb_txn,
                   struct lflow_input *input_data,
-                  struct hmap *lflows);
+                  struct lflow_data *lflow_data);
 bool lflow_handle_northd_ls_changes(struct ovsdb_idl_txn *ovnsb_txn,
                                     struct tracked_ls_changes *,
-                                    struct lflow_input *, struct hmap *lflows);
+                                    struct lflow_input *, struct lflow_data *);
 bool northd_handle_sb_port_binding_changes(
     const struct sbrec_port_binding_table *, struct hmap *ls_ports);
 
