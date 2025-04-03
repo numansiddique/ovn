c67fb7ec5954ca74befb9d738daf4c1b1bfff88a
Numan Siddique <numans@ovn.org>
2025-04-03 10:43:13 -0400

    controller: Expand the helper lport_get_peer().
    Expand the helper lport_get_peer() to get the peer for both
    'patch' and 'l3gateway' port binding types and add a new
    helper lport_get_patch_peer() for 'patch' type.
    
    This cleans up the code a bit.
    
    Acked-by: Han Zhou <hzhou@ovn.org>
    Acked-by: Mark Michelson <mmichels@redhat.com>
    Signed-off-by: Numan Siddique <numans@ovn.org>
    (cherry picked from commit cf7ecdcc8a552f08d937acaa834259f7e7846d28)

--- controller/lport.h
+++ controller/lport.h
@@ -71,6 +71,9 @@ lport_is_chassis_resident(struct ovsdb_idl_index *sbrec_port_binding_by_name,
 const struct sbrec_port_binding *lport_get_peer(
     const struct sbrec_port_binding *,
     struct ovsdb_idl_index *sbrec_port_binding_by_name);
+const struct sbrec_port_binding *lport_get_patch_peer(
+    const struct sbrec_port_binding *,
+    struct ovsdb_idl_index *sbrec_port_binding_by_name);
 const struct sbrec_port_binding *lport_get_l3gw_peer(
     const struct sbrec_port_binding *,
     struct ovsdb_idl_index *sbrec_port_binding_by_name);
