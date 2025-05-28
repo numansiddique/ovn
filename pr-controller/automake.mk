a283abfc238273e9f72c5e738e8268fa9b7d5490
Numan Siddique <numans@ovn.org>
2025-05-28 17:56:31 -0400

    ovn-pr-controller: Add bridge flow table manager.
    Signed-off-by: Numan Siddique <numans@ovn.org>

--- pr-controller/automake.mk
+++ pr-controller/automake.mk
@@ -1,5 +1,7 @@
 bin_PROGRAMS += pr-controller/ovn-pr-controller
 pr_controller_ovn_pr_controller_SOURCES = \
+	pr-controller/br-flow-mgr.c \
+	pr-controller/br-flow-mgr.h \
 	pr-controller/ovn-pr-controller.c \
 	pr-controller/en-lflow.c \
 	pr-controller/en-lflow.h \
